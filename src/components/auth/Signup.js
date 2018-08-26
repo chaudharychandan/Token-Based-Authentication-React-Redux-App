1f3a366083679432782766db815791cc86a4b87e
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-26 17:45:17 +0530

    Add redux store and integrate within the app

---
+++ src/components/auth/Signup.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Signup extends Component {
+  render() {
+    return (
+      <form>
+        <fieldset>
+          <label>Email</label>
+        </fieldset>
+        <fieldset>
+          <label>Password</label>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+export default Signup;
