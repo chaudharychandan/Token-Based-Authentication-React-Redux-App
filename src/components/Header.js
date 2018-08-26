3aa4a51de7e4289600ef6235f0be2a46bb24642c
Chandan Kumar <chandanchaudhary@outlook.com>
2018-08-27 01:06:50 +0530

    Change header on the basis of user is logged in or logged out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderLinks = () => {
+    if (this.props.authenticated) {
+      return (
+        <Fragment>
+          <Link to="/feature">Feature</Link>
+          <Link to="/signout">Sign Out</Link>
+        </Fragment>
+      );
+    } else {
+      return (
+        <Fragment>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </Fragment>
+      );
+    }
+  }
+
   render() {
     return (
       <div>
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+        {this.renderLinks()}
       </div>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = ({ auth: { authenticated } }) => ({
+  authenticated
+});
+
+export default connect(mapStateToProps)(Header);
